0b8e757fc7cf278b1f14836d96f80c18aff4c106
p77921354 <asliyildirim@std.iyte.edu.tr>
2020-10-20 09:49:17 +0000

    3

--- lab2/3.py
+++ lab2/3.py
@@ -3,6 +3,6 @@ b = 6
 c = -20
 print("your equation is " ,a,"x^2+",b,"x+",c)
 
-root_1 = (-b + (b**0.5 - 4*a*c)**0.5)/ 2*a
-root_2 = (-b - (b**0.5 - 4*a*c)**0.5)/ 2*a
+root_1 = (-b -(b**2 - 4*a*c)**0.5) / (2*a)
+root_2 = (-b + (b**2 - 4*a*c)** 0.5) / (2*a)
 print("root 1: ", root_1,"root 2: ", root_2)
