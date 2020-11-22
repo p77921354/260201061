6b8b8c8451b953c0f5b3a156f4e34748e0cf291f
p77921354 <asliyildirim@std.iyte.edu.tr>
2020-11-22 20:14:16 +0000

    practice for loop

--- practice/for_2.py
+++ practice/for_2.py
@@ -1,6 +1,11 @@
-for i in (0, 1, 2, 3, 4, 5):
-  if i == 2 or i == 4:
-    continue
-  print(i)
-  print("is it really working")
+# for i in (0, 1, 2, 3, 4, 5):
+#   if i == 2 or i == 4:
+#     continue
+#   print(i)
+#   print("is it really working")
   #continue is used for passing to the next iteration
+my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+for i in range(len(my_list)-1):
+  if i % 2 != 0:
+    print(my_list[i])
