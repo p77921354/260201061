4ba8311b16b3a298f95e5148c84370cad56a19be
p77921354 <asliyildirim@std.iyte.edu.tr>
2020-11-23 10:04:40 +0000

    1

---
+++ lab6/1.py
@@ -0,0 +1,12 @@
+correct_email = "ceng113@example.com"
+user_email = input("your email: ")
+
+user_email_lower = user_email.lower()
+
+two_parts = user_email.split("@")
+#print(two_parts)
+first_part = two_parts[0].replace(".","")
+
+email = first_part + "@" + two_parts[1]
+
+print(email == correct_email)
