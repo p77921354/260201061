97e9007220b3ce3597ed99d0875258c20e463bef
p77921354 <asliyildirim@std.iyte.edu.tr>
2020-12-26 12:14:07 +0000

    lab5 1

--- lab5/1.py
+++ lab5/1.py
@@ -1,1 +1,3 @@
-n = int(input("how may numbers: "))
+num = input("Enter an integer: ")
+for i in range(1,11):
+  print(num + " x " + str(i) + " = ", int(num) * i )
---
+++ lab7/4.py
@@ -0,0 +1,8 @@
+num = int(input("how many employees: "))
+employee = dict()
+
+while num != 0:
+  employee_name = input("name of the employee: ")
+  employee_salary = int(input("employee salary: "))
+
+
